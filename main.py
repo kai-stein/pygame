a882fc80b13f41b6558591075f54de9421f3716b
kai-stein <kai_holstein@hotmail.co.uk>
2025-11-06 10:28:11 +0000

    logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import player
+from logger import log_state
 
 def main():
     print("Starting Asteroids!")
@@ -24,6 +25,7 @@ def main():
     my_player = player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
